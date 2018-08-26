c7a50f8c9035c6cdfb7f1638fafd973b37d61b37
GameForPeople <koreagamemaker@gmail.com>
2018-08-26 19:42:16 +0900

    [DEV] Adjust "Screen-Setting" // DEV_4
    [DEV_4] Screen Setting 적용

---
+++ HSLD/Assets/Scenes/LoginScene/ScreenManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour {
+
+    void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;  // 화면이 꺼지지 않도록 설정
+        Screen.SetResolution(1280, 720, true);          // 해상도 설정 1280 - 720 , 전체화면 동일
+    }
+
+}
