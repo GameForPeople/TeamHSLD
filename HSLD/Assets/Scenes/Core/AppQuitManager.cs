6d773079fdde2e2d780025feec30b6d0b76e802e
GameForPeople <koreagamemaker@gmail.com>
2018-09-13 22:15:26 +0900

    [DEV] Adjust "AppQuitManager" // DEV_21
    [DEV_21] AppQuitManager 작업 및 GameCores에 추가

---
+++ HSLD/Assets/Scenes/Core/AppQuitManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppQuitManager : MonoBehaviour {
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; //play모드를 false로.
+                                                         //#elif UNITY_WEBPLAYER
+                                                         //        Application.OpenURL("http://google.com"); //구글웹으로 전환
+        #else
+            Application.Quit(); //어플리케이션 종료
+        #endif
+    }
+}
