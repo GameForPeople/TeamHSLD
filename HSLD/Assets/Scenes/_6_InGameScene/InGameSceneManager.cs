9bfde01e21f6d2d0ba5213151cba9ee5b7c4124a
GameForPeople <koreagamemaker@gmail.com>
2018-09-21 20:38:07 +0900

    [DEV] Adjust "InGameNetwork in Client 's Interface"  // DEV_25
    [DEV_25] 클라이언트에서 InGameScene 처리( 클라이언트 인터페이스 제공 )

--- HSLD/Assets/Scenes/_6_InGameScene/InGameSceneManager.cs
+++ HSLD/Assets/Scenes/_6_InGameScene/InGameSceneManager.cs
@@ -18,13 +18,37 @@ public class InGameSceneManager : MonoBehaviour {
     // 사용된 이벤트카드의 타입(인덱스) 입니다.
     public int          network_eventCardType;
 
+    private NetworkManager networkManager;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start ()
+    {
+        networkManager = GameObject.Find("GameCores").transform.Find("NetworkManager").GetComponent<NetworkManager>();
+        networkManager.inGameSceneManager = GameObject.Find("InGameSceneManager").GetComponent<InGameSceneManager>(); // same , return this;
+
+        network_recvProtocolFlag = 0;
+        network_terrainType = 0;
+        network_changeTerrainCount = 0;
+        network_eventCardType = 0;
+    }
+
+    void SendData (int InMsg)
+    {
+        // 이부분에서 일정확률로 동기화 안될 수도 있음. // 일부 공격자 메세지 생략될 수 있음.
+        network_recvProtocolFlag = 0;
+
+        networkManager.SendData(InMsg);
+    }
+
+    // 동기화가 된다는 가능성
+    int GetRecvProtocol()
+    {
+        if (network_recvProtocolFlag == 0)
+        {
+            // 네트워크 지연처리 필요함.
+            // 1. 상대방 - 서버
+            // 2. 서버 - 나...
+        }
+
+        return network_recvProtocolFlag;
+    }
 }
