4a581ce9187a481533d0e448a8bb6f2252c763b1
ysk1965 <enter_maintanance@naver.com>
2018-10-06 04:00:12 +0900

    #[181006] - Client - 메시 컨트롤 수정 - 메시 관리 수정

--- HSLD/Assets/Scenes/_6_InGameScene/CameraController.cs
+++ HSLD/Assets/Scenes/_6_InGameScene/CameraController.cs
@@ -60,6 +60,8 @@ public class CameraController : MonoBehaviour
         float fdistance = normalDirection.magnitude;
         normalDirection = Vector3.Normalize(normalDirection);
 
+        //DiceCount = GetComponent<DiceSystem>().publicDiceNum;
+
         Touch[] touches = Input.touches;
 
         if (mainCamera)
@@ -86,12 +88,15 @@ public class CameraController : MonoBehaviour
 
                     if (ChangeableCount > 0 && ChangeableCount < DiceCount)
                     {
-                        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                        if (Input.GetTouch(0).phase == TouchPhase.Moved && offset < 1)
                         {
                             if (!PickedMeshObj.GetComponent<MeshController>().isFixed) // 정해져있지 않음, 턴이 지나면 Fixed로 바꿔주는 게 필요
                             {
-                                PickedMeshObj.GetComponent<MeshController>().isAwake = true; // 깨어나면 계산 후 다시 잠듦
-                                
+                                if (!PickedMeshObj.GetComponent<MeshController>().isAwake)
+                                {
+                                    PickedMeshObj.GetComponent<MeshController>().isAwake = true; // 깨어나면 계산 후 다시 잠듦
+                                    offset = 3;
+                                }
                             }
                         }
                     }
@@ -135,6 +140,35 @@ public class CameraController : MonoBehaviour
                 }
                 mainCamera.transform.LookAt(MyPlanet);
             }
+
+
+            // 마우스 클릭
+            if (Input.GetMouseButton(0))
+            {
+                Vector3 mos = Input.mousePosition;
+                mos.z = mainCamera.farClipPlane;
+            
+                Vector3 dir = mainCamera.ScreenToWorldPoint(mos);
+            
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, dir, out hit, mos.z))
+                {
+                    PickedMeshObj = GameObject.Find(hit.transform.name);
+                    if (PickedMeshObj)
+                    {
+                        // 정해져있지 않음, 턴이 지나면 Fixed로 바꿔주는 게 필요
+                        if (!PickedMeshObj.GetComponent<MeshController>().isFixed && offset < 1) 
+                        {
+                            if (!PickedMeshObj.GetComponent<MeshController>().isAwake)
+                            {
+                                PickedMeshObj.GetComponent<MeshController>().isAwake = true; // 깨어나면 계산 후 다시 잠듦
+                                offset = 3;
+                            }
+                        }
+                    }
+                }
+            }
         }
+
     }
 }
