5dfa6aa08046ee8606e8ba9401bd1aabf325c3c0
Yoo Sang Keon <enter_maintanance@naver.com>
2018-10-16 20:33:35 +0900

    # [181016] Client - 지형선택 인접타일은 매쉬와 매쉬사이 거리 비교해서 가장 가까운 3곳 을 넣어봤는데, 구 자체가 울퉁불퉁...? 해서 임의로 들어가는 부분 있어요. 그래서 그냥 노가다로 넣어야 할듯합니다. (내일까지 제가 할게욥) - 드래그는 모바일 환경에서 touch moved 활용할 예정이라 클라이언트 환경에서는 우선 안될거 같아요 - 다음턴에 이전에 턴count 변수값을 불러오는것같아요. --> 카드 픽 한 후에 count들어오기 시작. - 지형 피형 픽할 때만 지형 피킹 수정 - 선택 안되는 곳 수정

--- HSLD/Assets/Scenes/_6_InGameScene/MeshController.cs
+++ HSLD/Assets/Scenes/_6_InGameScene/MeshController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public enum Terrain{
     DEFAULT,
@@ -22,6 +23,14 @@ public class MeshController : MonoBehaviour {
     public bool isAwake;
     public bool isFixed;
     public bool isMine;
+    public GameObject[] Link;
+
+    private int[] iLink;
+    private float[] fLink;
+    
+    private bool once;
+
+
     // Use this for initialization
 
     void Start () {
@@ -31,76 +40,102 @@ public class MeshController : MonoBehaviour {
         isAwake = false;
         isFixed = false;
         isMine = false;
+        once = true;
         giveNumber++;
         MeshNumber = giveNumber;
         name = giveNumber.ToString();
+
+        Link = new GameObject[3];
+        iLink = new int[3];
+        fLink = new float[3];
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //Debug.Log(SceneManager.GetComponent<CardSystem>().pickedCard);
-
         if (isAwake)
         {
-            if (terrainstate == Terrain.DEFAULT) // 기본 터레인인거면 아무도 안건들였음을 의미 함.
+            if (once == true)
             {
-                if (CameraController.ChangeableCount > 0)
+                float min = 999;
+                for (int i = 1; i < AllMeshController.myPlanet.GetComponent<AllMeshController>().AllContainer.Length; i++)
                 {
-                    // 임시
-                    setBarren();
-                    CameraController.ChangeableCount--;
+                    if (i == MeshNumber) continue;
 
-                    if (AllMeshController.IngameManager.GetComponent<CardSystem>().pickedCard)
+                    Vector3 otherMeshPosition = AllMeshController.myPlanet.GetComponent<AllMeshController>().AllContainer[i].transform.position;
+                    Vector3 pointTopoint = transform.position - otherMeshPosition; // 내 포지션과 다른 포지션을 빼
+                    float distance = pointTopoint.magnitude;
+
+                    if (distance < 12.6)
                     {
-                        Debug.Log("picked");
-                        GameObject picked = AllMeshController.IngameManager.GetComponent<CardSystem>().pickedCard;
-
-                        if (picked.name.Equals("TerrainCardImg1"))
+                        for (int j = 0; j < 3; j++)
                         {
-                            Debug.Log("TerrainCardImg1");
-                            setBarren();
-                        }
-                        else if (picked.name.Equals("TerrainCardImg2"))
-                        {
-                            Debug.Log("TerrainCardImg2");
-                            setModeration();
-                        }
-                        else if (picked.name.Equals("TerrainCardImg3"))
-                        {
-                            Debug.Log("TerrainCardImg3");
-                            setCold();
-                        }
-                        else if (picked.name.Equals("TerrainCardImg4"))
-                        {
-                            Debug.Log("임시");
-                            setCold();
-                        }
-                        else if (picked.name.Equals("TerrainCardImg5"))
-                        {
-                            Debug.Log("임시");
-                            setCold();
-                        }
-                        else if (picked.name.Equals("EventCardImg1"))
-                        {
-                            Debug.Log("EventCardImg1");
-                            setSea();
-                        }
-                        else if (picked.name.Equals("EventCardImg2"))
-                        {
-                            Debug.Log("EventCardImg2");
-                            setMountain();
+                            if (Link[j] == null)
+                            {
+                                Link[j] = GameObject.Find(i.ToString());
+                                Debug.Log(distance);
+                                break;
+                            }
                         }
                     }
-                   
+                }
+
+
+                once = false;
+            }
+
+            if (terrainstate == Terrain.DEFAULT) // 기본 터레인인거면 아무도 안건들였음을 의미 함.
+            {
+                // 임시
+
+                if (AllMeshController.IngameManager.GetComponent<CardSystem>().pickedCard)
+                {
+                    Debug.Log("picked");
+                    GameObject picked = AllMeshController.IngameManager.GetComponent<CardSystem>().pickedCard;
+
+                    CameraController.ChangeableCount--;
+
+                    if (picked.name.Equals("TerrainCardImg1"))
+                    {
+                        Debug.Log("TerrainCardImg1");
+                        setBarren();
+                    }
+                    else if (picked.name.Equals("TerrainCardImg2"))
+                    {
+                        Debug.Log("TerrainCardImg2");
+                        setModeration();
+                    }
+                    else if (picked.name.Equals("TerrainCardImg3"))
+                    {
+                        Debug.Log("TerrainCardImg3");
+                        setCold();
+                    }
+                    else if (picked.name.Equals("TerrainCardImg4"))
+                    {
+                        Debug.Log("임시");
+                        setCold();
+                    }
+                    else if (picked.name.Equals("TerrainCardImg5"))
+                    {
+                        Debug.Log("임시");
+                        setCold();
+                    }
+                    else if (picked.name.Equals("EventCardImg1"))
+                    {
+                        Debug.Log("EventCardImg1");
+                        setSea();
+                    }
+                    else if (picked.name.Equals("EventCardImg2"))
+                    {
+                        Debug.Log("EventCardImg2");
+                        setMountain();
+                    }
                 }
             }
             else // 그 외 지형카드로 색을 칠했었던거라면 다시 돌려줌
             {
-                if (CameraController.ChangeableCount < CameraController.DiceCount) {
-                    GetComponent<MeshRenderer>().material = defaultMaterial;
-                    terrainstate = Terrain.DEFAULT;
-                    CameraController.ChangeableCount++;
-                }
+                GetComponent<MeshRenderer>().material = defaultMaterial;
+                terrainstate = Terrain.DEFAULT;
+                CameraController.ChangeableCount++;
             }
 
             isAwake = false;
