7ef5e6943acb3899069150bd49b433a8d56a770d
DESKTOP-J66658U\1seconds <1seconds>
2018-10-22 19:17:27 +0900

    sorry... bro

--- HSLD/Assets/Scripts/TurnSystem.cs
+++ HSLD/Assets/Scripts/TurnSystem.cs
@@ -58,8 +58,10 @@ public class TurnSystem : MonoBehaviour
     IEnumerator DisplayTextMessageCor(string value, float time)
     {
         mainTxt.text = value;
+        GameObject.FindWithTag("GameManager").GetComponent<FlowSystem>().tmpAnimationImage.SetActive(true);
         yield return new WaitForSeconds(time);
         mainTxt.text = "";
+        GameObject.FindWithTag("GameManager").GetComponent<FlowSystem>().tmpAnimationImage.SetActive(false);
     }
 
     IEnumerator ReadySetOrderTimer()
