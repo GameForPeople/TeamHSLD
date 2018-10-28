04132c716c934b8dccbc9203e136846f9deeadb0
GameForPeople <koreagamemaker@gmail.com>
2018-10-28 18:44:45 +0900

    [DEV] Adjust "UDP Socket and thread in client" // DEV_54
    클라이언트 UDP 소켓 적용
     - C#, 비동기 방식으로, 1초에 한번씩 Recv여부 검사하도록 설정

--- HSLD/Assets/Scenes/Core/NetworkManager.cs
+++ HSLD/Assets/Scenes/Core/NetworkManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿//#define UDP
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -566,6 +568,51 @@ public class NetworkManager : MonoBehaviour
         recvType = 0;
     }
 
+#if UDP
+    public class UDP_StateObject
+    {
+       public UdpClient u;
+       public IPEndPoint e;
+    }
+
+    public static bool messageReceived = false;
+
+    public static void UDP_ReceiveCallback(IAsyncResult ar)
+    {
+        UdpClient u = (UdpClient)((UDP_StateObject)(ar.AsyncState)).u;
+        IPEndPoint e = (IPEndPoint)((UDP_StateObject)(ar.AsyncState)).e;
+
+        Byte[] receiveBytes = u.EndReceive(ar, ref e);
+        string receiveString = Encoding.ASCII.GetString(receiveBytes);
+
+        Console.WriteLine("Received: {0}", receiveString);
+        messageReceived = true;
+    }
+
+    public static void UDP_Receive()
+    {
+        // Receive a message and write it to the console.
+        IPEndPoint e = new IPEndPoint(IPAddress.Any, 9001);
+        UdpClient u = new UdpClient(e);
+
+        UDP_StateObject s = new UDP_StateObject();
+        s.e = e;
+        s.u = u;
+
+        //Console.WriteLine("listening for messages");
+        u.BeginReceive(new AsyncCallback(UDP_ReceiveCallback), s);
+
+        // Do some work while we wait for a message. For this example,
+        // we'll just sleep
+        while (!messageReceived)
+        {
+            Thread.Sleep(1000);
+        }
+    }
+    
+#endif
+
+
     public void ParsingServerIP()
     {
         StartCoroutine(ParsingServerIPCoroutine());
@@ -617,8 +664,6 @@ public class NetworkManager : MonoBehaviour
         }
     }
 }
-
-
 
 enum PROTOCOL : int
 {
