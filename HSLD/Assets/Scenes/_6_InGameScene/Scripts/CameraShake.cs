8337663bf88dbfe36f5f93ba0e8890b00aea76cf
Yoo Sang Keon <enter_maintanance@naver.com>
2018-11-16 10:56:00 +0900

    [181116] Client - CameraShake2
    - 스카이박스에 구체만 띄워져 있는 상황이라 쉐이크 느낌이 나지 않아서 UI까지 흔들도록

--- HSLD/Assets/Scenes/_6_InGameScene/Scripts/CameraShake.cs
+++ HSLD/Assets/Scenes/_6_InGameScene/Scripts/CameraShake.cs
@@ -2,11 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour {
+public class CameraShake : MonoBehaviour
+{
+
+    public GameObject canvas;
+    public List<RectTransform> UIList;
+    Vector2[] originList;
+
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+
+        canvas.GetComponentsInChildren<RectTransform>(UIList);
+        //Debug.Log(canvas.GetComponentsInChildren<RectTransform>());
+        //UIList.RemoveAt(0);
+
+        originList = new Vector2[UIList.Count];
+    }
 
     public IEnumerator Shake(float duration, float magnitude)
     {
         Vector3 originPos = transform.localPosition;
+        for (int i = 0; i < UIList.Count; i++)
+        {
+            originList[i] = UIList[i].transform.localPosition;
+        }
 
         float elapsed = 0.0f;
 
@@ -17,13 +37,26 @@ public class CameraShake : MonoBehaviour {
             float z = Random.Range(-1f, 1f) * magnitude;
 
             transform.localPosition = new Vector3(originPos.x + x, originPos.y + y, originPos.z + z);
-            Debug.Log(transform.localPosition);
+            //Debug.Log(transform.localPosition);
 
             elapsed += Time.deltaTime;
+
+            for(int i = 0; i < UIList.Count; i++)
+            {
+                UIList[i].transform.localPosition = new Vector2(originList[i].x + x*2, originList[i].y + y*2);
+                if(i == 1)
+                {
+                    Debug.Log(UIList[i].transform.localPosition);
+                }
+            }
 
             yield return null;
         }
 
         transform.localPosition = originPos;
+        for (int i = 0; i < UIList.Count; i++)
+        {
+            UIList[i].transform.localPosition = originList[i];
+        }
     }
 }
