afcb5d00f5afe93b9d37a9e7628a63c1aaacbe6b
usung <1seconds@naver.com>
2019-01-17 12:19:08 +0900

    [DEV] Sound BugFix Ver55.

--- HSLD/Assets/Scripts/SoundBar.cs
+++ HSLD/Assets/Scripts/SoundBar.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SoundBar : EventTrigger
 {
+
+    private SoundManager soundmanager;
+
+
     public override void OnPointerClick(PointerEventData data)
     {
-        GameObject.Find("SoundManager").GetComponent<SoundManager>().SoundVolumeControl(gameObject);
+        soundmanager.SoundVolumeControl(gameObject);
     }
 
     private void OnEnable()
     {
+        soundmanager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        if(gameObject.transform.parent.name.Contains("BGM"))
+        {
+            if (soundmanager.BGMSource[0].volume * 8 >= int.Parse(gameObject.name) - 1)
+                gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            else
+                gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        }
+        else
+        {
+            if (soundmanager.SFXSource.volume * 8 >= int.Parse(gameObject.name) - 1)
+                gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            else
+                gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        }
+        
         Time.timeScale = 0.0f;
     }
 
