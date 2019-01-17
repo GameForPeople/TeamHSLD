5f1643765d31d8f3e24c170b87550a6b609c6ed8
GameForPeople <koreagamemaker@gmail.com>
2019-01-18 01:51:26 +0900

    fix bitwise-operator
    비트연산 잘못하던 부분 깔짝 수정

--- Server/Server/ContentManager/CharacterManager.h
+++ Server/Server/ContentManager/CharacterManager.h
@@ -5,7 +5,8 @@
 
 class CharacterManager
 {
-	enum CHARACTER_INDEX : int	// for Bit
+public:
+	enum CHARACTER_INDEX : int
 	{
 		MY_PLANET = 0
 		, VICTORY = 1
@@ -16,6 +17,19 @@ class CharacterManager
 		, FIRE = 6
 		, ICE = 7
 		, TREE = 8
+	};
+
+	enum class CHARACTER_BIT : int
+	{
+		MY_PLANET = 0
+		, VICTORY = 0x00000001
+		, SUN = 0x00000002
+		, SOIL = 0x00000004
+		, GRASS = 0x00000008
+		, WATER = 0x00000010
+		, FIRE = 0x00000020
+		, ICE = 0x00000040
+		, TREE = 0x00000080
 	};
 
 	enum class CHARACTER_PRICE : int
@@ -31,8 +45,9 @@ class CharacterManager
 		, TREE = 1000
 	};
 
+private:
 	const int			characterCount;
-	std::vector<int>	characterPriceCont;
+	std::vector<std::pair<CHARACTER_BIT,  CHARACTER_PRICE>>	characterPriceCont;
 
 public:
 	CharacterManager()
@@ -40,16 +55,17 @@ public:
 	{
 		characterPriceCont.reserve(characterCount + 1);
 		characterPriceCont.emplace_back(0);	// index - 0 buffer;
-		characterPriceCont.emplace_back(static_cast<int>(CHARACTER_PRICE::VICTORY));
-		characterPriceCont.emplace_back(static_cast<int>(CHARACTER_PRICE::SOIL));
-		characterPriceCont.emplace_back(static_cast<int>(CHARACTER_PRICE::GRASS));
-		characterPriceCont.emplace_back(static_cast<int>(CHARACTER_PRICE::WATER));
-		characterPriceCont.emplace_back(static_cast<int>(CHARACTER_PRICE::FIRE));
-		characterPriceCont.emplace_back(static_cast<int>(CHARACTER_PRICE::ICE));
-		characterPriceCont.emplace_back(static_cast<int>(CHARACTER_PRICE::TREE));
+		characterPriceCont.emplace_back(make_pair<CHARACTER_BIT, CHARACTER_PRICE>(CHARACTER_BIT::VICTORY, CHARACTER_PRICE::VICTORY));
+		characterPriceCont.emplace_back(make_pair<CHARACTER_BIT, CHARACTER_PRICE>(CHARACTER_BIT::SOIL, CHARACTER_PRICE::SOIL));
+		characterPriceCont.emplace_back(make_pair<CHARACTER_BIT, CHARACTER_PRICE>(CHARACTER_BIT::GRASS, CHARACTER_PRICE::GRASS));
+		characterPriceCont.emplace_back(make_pair<CHARACTER_BIT, CHARACTER_PRICE>(CHARACTER_BIT::WATER, CHARACTER_PRICE::WATER));
+		characterPriceCont.emplace_back(make_pair<CHARACTER_BIT, CHARACTER_PRICE>(CHARACTER_BIT::FIRE, CHARACTER_PRICE::FIRE));
+		characterPriceCont.emplace_back(make_pair<CHARACTER_BIT, CHARACTER_PRICE>(CHARACTER_BIT::ICE, CHARACTER_PRICE::ICE));
+		characterPriceCont.emplace_back(make_pair<CHARACTER_BIT, CHARACTER_PRICE>(CHARACTER_BIT::TREE, CHARACTER_PRICE::TREE));
 	};
 	~CharacterManager() = default;
 public:
 	_NODISCARD __inline constexpr int GetCharacterCount() const noexcept { return characterCount; }
-	_NODISCARD __inline int GetCharacterPriceWithIndex(const int InIndex) const noexcept { return characterPriceCont[InIndex]; };
+	_NODISCARD __inline CHARACTER_BIT GetCharacterBitWithIndex(const int InIndex) const noexcept { return characterPriceCont[InIndex].first; };
+	_NODISCARD __inline CHARACTER_PRICE GetCharacterPriceWithIndex(const int InIndex) const noexcept { return characterPriceCont[InIndex].second; };
 };
