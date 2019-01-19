cb5d460010a8f573a8d34593de130d7cd919151c
GameForPeople <koreagamemaker@gmail.com>
2019-01-20 01:19:57 +0900

    re-Assign "Setting UI in MainUiScene"
    세팅 UI 경로 관련 변경 적용 건

---
+++ HSLD/Assets/Scenes/_3_MainUIScene/MainUISceneFXManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainUISceneFXManager : MonoBehaviour {
+
+    public ParticleSystem successParticle;
+    public ParticleSystem failParticle;
+
+    // Use this for initialization
+    void Start () {
+        GameObject.Find("FX_Canvas").GetComponent<Canvas>().worldCamera 
+            = GameObject.Find("GameCores").transform.Find("ClientBase_Space").transform.Find("ClientBase_Camera").GetComponent<Camera>();
+	}
+
+    public void RenderSuccessOrFailParticle(bool InisSuccessOrFail)
+    {
+        if (InisSuccessOrFail)
+        {
+            //Instantiate(successParticle, transform.position, Quaternion.identity);
+            GameObject.Find("FX_Canvas").transform.Find("SuccessParticle").gameObject.SetActive(true);
+        }
+        else
+        {
+            GameObject.Find("FX_Canvas").transform.Find("FailParticle").gameObject.SetActive(true);
+        }
+    }
+}
