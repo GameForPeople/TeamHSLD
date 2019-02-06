e2250b092aa9e8ed3078a0bb407a2360a2e2bd9b
DESKTOP-C133V5O\DELL <enter_maintanance@naver.com>
2019-02-06 13:46:46 +0900

    [FIX] fix PinchCamera
    - 카메라 수정중입니다.

--- HSLD/Assets/Scenes/_6_InGameScene/Scripts/CameraController.cs
+++ HSLD/Assets/Scenes/_6_InGameScene/Scripts/CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public Transform MyPlanet;
     public Camera mainCamera;
-    private Vector2 PrevPoint;
+    private Vector2 rotateAmount;
 
     public float orthoZoomSpeed;
     public float minDistance;
@@ -40,15 +40,18 @@ public class CameraController : MonoBehaviour
         Vector3 normalDirection = myTransform.position - MyPlanet.position;
         float fdistance = normalDirection.magnitude;
 
-        VerticalVelue += 15;
+        //Debug.Log("VerticalVelue : " + VerticalVelue);
+
         if (isUp)
         {
+            VerticalVelue += 15;
             myTransform.localRotation = Quaternion.Euler(myTransform.localRotation.y + HorizontalValue, myTransform.localRotation.x + VerticalVelue, 0);
             myTransform.localPosition = MyPlanet.transform.position - (myTransform.rotation * Vector3.forward * fdistance);
         }
         else
         {
-            myTransform.localRotation = Quaternion.Euler(myTransform.localRotation.y + HorizontalValue, myTransform.localRotation.x - VerticalVelue, 0);
+            VerticalVelue -= 15;
+            myTransform.localRotation = Quaternion.Euler(myTransform.localRotation.y + HorizontalValue, myTransform.localRotation.x + VerticalVelue, 0);
             myTransform.localPosition = MyPlanet.transform.position - (myTransform.rotation * Vector3.forward * fdistance);
         }
     }
@@ -58,15 +61,17 @@ public class CameraController : MonoBehaviour
         Vector3 normalDirection = myTransform.position - MyPlanet.position;
         float fdistance = normalDirection.magnitude;
 
-        HorizontalValue += 15;
+        //Debug.Log("HorizontalValue : " + HorizontalValue);
 
         if (isRight)
         {
-            myTransform.localRotation = Quaternion.Euler(myTransform.localRotation.y - HorizontalValue, myTransform.localRotation.x + VerticalVelue, 0);
+            HorizontalValue -= 15;
+            myTransform.localRotation = Quaternion.Euler(myTransform.localRotation.y + HorizontalValue, myTransform.localRotation.x + VerticalVelue, 0);
             myTransform.localPosition = MyPlanet.transform.position - (myTransform.rotation * Vector3.forward * fdistance);
         }
         else
         {
+            HorizontalValue += 15;
             myTransform.localRotation = Quaternion.Euler(myTransform.localRotation.y + HorizontalValue, myTransform.localRotation.x + VerticalVelue, 0);
             myTransform.localPosition = MyPlanet.transform.position - (myTransform.rotation * Vector3.forward * fdistance);
         }
@@ -82,10 +87,11 @@ public class CameraController : MonoBehaviour
                 yield break; // 선택 매쉬가 달라졌으면 off
             }
             
-            if (Mathf.Abs((transform.position - end).magnitude) < 1.0f) yield break; // 보간 제한
+            if (Mathf.Abs((transform.position - end).magnitude) < 1.5f) yield break; // 보간 제한
             
 
-            transform.position = Vector3.Lerp(transform.position, end, Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, end, Time.deltaTime * 2.5f);
+
             Camera.main.transform.LookAt(MyPlanet);
             yield return null;
         }
@@ -110,6 +116,7 @@ public class CameraController : MonoBehaviour
             if (fdistance >= minDistance || fdistance <= maxDistance)
             {
                 myTransform.position = priorPosition;
+                //myTransform.LookAt(MyPlanet);
             } // 확대축소 관련
 
             if (Input.touchCount == 2) // 확대축소 스와이프
@@ -123,38 +130,14 @@ public class CameraController : MonoBehaviour
                 float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
                 float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-                float deltaMagnitudediff = prevTouchDeltaMag - touchDeltaMag;
+                float deltaMagnitudediff = touchDeltaMag - prevTouchDeltaMag;
 
                 if (fdistance < minDistance && fdistance > maxDistance)
                 {
                     priorPosition = myTransform.position;
                     myTransform.position = myTransform.position - -(normalDirection * deltaMagnitudediff * orthoZoomSpeed);
                 }
-
-
-                if (Input.GetTouch(0).phase == TouchPhase.Moved)
-                {
-                    PrevPoint += (Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) * Time.deltaTime / 10000;
-                    //Debug.Log(PrevPoint);
-
-                    //Vector3 position = MyPlanet.position - (rotation * Vector3.forward * fdistance);
-
-                    myTransform.localRotation = Quaternion.Euler((Input.GetTouch(0).position.y - PrevPoint.y), 
-                        (Input.GetTouch(0).position.x - PrevPoint.x), 0);
-                    myTransform.localPosition = MyPlanet.position - (myTransform.rotation * Vector3.forward * fdistance);
-
-                    /*
-                    mainCamera.transform.RotateAround(MyPlanet.position, Vector3.left,
-                        (Input.GetTouch(0).position.y - PrevPoint.y) );
-
-                    mainCamera.transform.RotateAround(MyPlanet.position, Vector3.up,
-                        (Input.GetTouch(0).position.x - PrevPoint.x) * RotationSensitivity);
-                    */
-                    //PrevPoint = Input.GetTouch(0).position;
-                }
             }
-            //PrevPoint.Set(0, 0);
-            myTransform.LookAt(MyPlanet);
         }
 
         if (Input.touchCount == 1 && CameraController.offset < 0.5)
@@ -162,9 +145,27 @@ public class CameraController : MonoBehaviour
             Camera.main.GetComponent<PCverPIcking>().Picked(true);
         }
 
-        if (Input.touchCount == 4 && CameraController.offset < 0.5)
+        if (Input.touchCount == 3)
         {
-            MyPlanet.GetComponent<AllMeshController>().CleanPickContainer(); 
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                rotateAmount += (Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) * Time.deltaTime / 10000;
+
+                //Vector3 position = MyPlanet.position - (rotation * Vector3.forward * fdistance);
+                
+                myTransform.localRotation = Quaternion.Euler((Input.GetTouch(0).position.y - rotateAmount.y),
+                    (Input.GetTouch(0).position.x - rotateAmount.x), 0);
+                myTransform.localPosition = MyPlanet.position - (myTransform.rotation * Vector3.forward * fdistance);
+                
+                /*
+                mainCamera.transform.RotateAround(MyPlanet.position, Vector3.left,
+                    (Input.GetTouch(0).position.y - PrevPoint.y) );
+
+                mainCamera.transform.RotateAround(MyPlanet.position, Vector3.up,
+                    (Input.GetTouch(0).position.x - PrevPoint.x) * RotationSensitivity);
+                */
+                //PrevPoint = Input.GetTouch(0).position;
+            }
         }
     }
 }
