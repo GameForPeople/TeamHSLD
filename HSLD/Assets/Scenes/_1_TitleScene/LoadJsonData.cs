51026cf0aaf805f1ce2a205c2a72e3a5d48c1a9f
DESKTOP-C133V5O\DELL <enter_maintanance@naver.com>
2019-02-09 22:05:58 +0900

    [DEV] Setting Json
    - 세이브, 로드 데이터

--- HSLD/Assets/Scenes/_1_TitleScene/LoadJsonData.cs
+++ HSLD/Assets/Scenes/_1_TitleScene/LoadJsonData.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using LitJson;
 
 public class LoadJsonData : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public List<MeshInfo> settingMesh = new List<MeshInfo>();
+    
+    void Start () {
+        LoadMeshData();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    
+    public void LoadMeshData()
+    {
+        Debug.Log("LoadJsonData");
+
+        if(File.Exists(Application.dataPath+ "/Resources/Data/MeshInfoData.json"))
+        {
+            string jsonStr = File.ReadAllText(Application.dataPath + "/Resources/Data/MeshInfoData.json");
+
+            Debug.Log(jsonStr);
+            JsonData MeshData = JsonMapper.ToObject(jsonStr);
+
+            for(int i = 1; i < 321; i++)
+            {
+
+            }
+        }
+        else
+        {
+            Debug.Log("non data");
+        }
+    }
 }
---
+++ HSLD/Assets/Scenes/_6_InGameScene/Scripts/SaveJsonData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+public class MeshInfo
+{
+    public string meshID;
+    public string meshState;
+
+    public MeshInfo(string _meshID, string _meshState)
+    {
+        meshID = _meshID;
+        meshState = _meshState;
+    }
+}
+
+public class SaveJsonData : MonoBehaviour {
+
+    public List<MeshInfo> meshInfoList = new List<MeshInfo>();
+
+	// Use this for initialization
+	void Start () {
+        SaveMeshData();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SaveMeshData()
+    {
+        Debug.Log("SaveJsonData");
+
+        for(int i = 1; i < 321; i++)
+        {
+            meshInfoList.Add(new MeshInfo(AllMeshController.myPlanet.GetComponent<AllMeshController>().AllContainer[i].GetComponent<MeshController>().name
+                , AllMeshController.myPlanet.GetComponent<AllMeshController>().AllContainer[i].GetComponent<MeshController>().terrainstate.ToString()));
+        }
+
+        JsonData infoJson = JsonMapper.ToJson(meshInfoList);
+
+        File.WriteAllText(Application.dataPath + "/Resources/Data/MeshInfoData.json", infoJson.ToString());
+    }
+}
