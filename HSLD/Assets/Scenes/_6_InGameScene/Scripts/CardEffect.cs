15e28773693ef3d85e8e490735f70dad0a798dd3
DESKTOP-C133V5O\DELL <enter_maintanance@naver.com>
2019-02-19 01:54:52 +0900

    [DEV] Eventcard Action
    - 특수카드 이펙트 적용

--- HSLD/Assets/Scenes/_6_InGameScene/Scripts/CardEffect.cs
+++ HSLD/Assets/Scenes/_6_InGameScene/Scripts/CardEffect.cs
@@ -4,38 +4,30 @@ using UnityEngine;
 
 public class CardEffect : MonoBehaviour {
     public GameObject[] cardObject;
-    public GameObject EffectObject;
+    public GameObject effectCard;
     public GameObject testMesh;
     Vector2 rotateAmount;
     float speed = 100;
-    float test = 0;
 
     // Use this for initialization
     void Start () {
-        setCard(cardObject[0], testMesh.transform);
-    }
-
-    private void Update()
-    {
-        test++;
-        if (test == 1000)
-        {
-            StartCoroutine(Camera.main.GetComponent<CameraController>().movePositionEffect(testMesh.transform.position));
-            StartCoroutine(movingCard(EffectObject));
-        }
-        
+        //setCard(cardObject[0], testMesh.transform);
     }
 
     public void setCard(GameObject cardObj, Transform transform)
     {
-        Vector3 firstPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        Quaternion firstQuaternion = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-        
-        EffectObject = Instantiate(cardObj, firstPosition, firstQuaternion);
+        Vector3 firstPosition = new Vector3(testMesh.transform.position.x, testMesh.transform.position.y - 30000, testMesh.transform.position.z);
+        Vector3 secondPosition = testMesh.transform.position;
 
+        Quaternion firstQuaternion = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+
+        effectCard = Instantiate(cardObj, firstPosition, firstQuaternion);
+
+        StartCoroutine(Camera.main.GetComponent<CameraController>().movePositionEffect(testMesh.transform.position));
+        StartCoroutine(moveUpCard(effectCard, firstPosition, secondPosition));
     }
 
-    public IEnumerator movingCard(GameObject cardObj)
+    public IEnumerator moveRotateCard(GameObject cardObj)
     {
         while (rotateAmount.x < 365.0f)
         {
@@ -44,5 +36,16 @@ public class CardEffect : MonoBehaviour {
             cardObj.transform.RotateAround(gameObject.transform.position, rotate * 120, speed * Time.deltaTime);
             yield return null;
         }
+    }
+
+    public IEnumerator moveUpCard(GameObject cardObj, Vector3 start, Vector3 end)
+    {
+        while (effectCard.transform.position.y < end.y -5)
+        {
+            effectCard.transform.position = Vector3.Lerp(effectCard.transform.position, end, Time.deltaTime);
+            yield return null;
+        }
+
+        StartCoroutine(moveRotateCard(effectCard));
     }
 }
